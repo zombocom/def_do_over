cf4dcbe713de2126e38018a890a717b3568eff58
schneems <richard.schneeman+foo@gmail.com>
2020-10-30 14:19:39 -0500

    WIP

--- spec/did_you_do_spec.rb
+++ spec/did_you_do_spec.rb
@@ -41,34 +41,74 @@ RSpec.describe DidYouDo do
       @source = source
       @array = []
 
-      hash = {}
-      @source.each_line.with_index do |line, i|
+      count_line_array = []
+      space_count_end_index_hash = Hash.new {|h, k| h[k] = [] }
+      @lines = @source.lines
+      @lines.each.with_index do |line, i|
+        count = line.split(/\w/).first.length
+        count_line_array << count
+
         next unless line.strip == "end"
-        spaces = line.split(/\w/).first.length
-        hash[spaces] ||= []
-        hash[spaces] << i
+        space_count_end_index_hash[count] << i
       end
 
-      indent = hash.keys.min
-      @array = hash[indent]
+
+      # start at the last end, and search up until finding a line that matches
+      # indentation
+      end_levels = space_count_end_index_hash.keys.sort
+      spaces = end_levels[0]
+
+      space_count_end_index_hash[spaces].each do |end_index|
+        middle = []
+        beginning = nil
+
+        end_index.pred.downto(0).each do |i|
+          line = @lines[i]
+
+          if count_line_array[i] == spaces
+            beginning = line
+            break
+          else
+            middle << line
+          end
+        end
+
+        @array << {ending: @lines[end_index], beginning: beginning, middle: middle.reverse.join($/)}
+      end
     end
 
     def length
       @array.length
     end
+
+    def to_array
+      @array
+    end
   end
 
   it "blerg" do
-
     source = <<~EOM
-      describe "things" do
+      describe "first" do
       end
 
       describe "next" do
+        Foo.new.call
       end
     EOM
 
-    out = ParseEndZones.new(source)
-    expect(out.length).to eq(2)
+    zones = ParseEndZones.new(source)
+    expect(zones.length).to eq(2)
+
+    zones.to_array.first.tap do |z|
+      expect(z[:beginning]).to include(%Q{describe "first" do})
+      expect(z[:middle]).to include(%Q{})
+      expect(z[:ending]).to include(%Q{end})
+    end
+
+    zones.to_array.last.tap do |z|
+      expect(z[:beginning]).to include(%Q{describe "next" do})
+      expect(z[:middle]).to include(%Q{Foo.new.call})
+      expect(z[:ending]).to include(%Q{end})
+    end
   end
 end
